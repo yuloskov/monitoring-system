7f9a11d5b9b6f5bf55d8e9c192c83771f2967d5e
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-04-02 17:31:35 +0300

    Add logo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../logo.svg';
 import { Link } from 'react-router-dom';
 
 
 function MyNavbar() {
   return (
     <Navbar bg="primary" variant="dark" expand="lg" sticky="top">
-      <Link to='/'><Navbar.Brand>START metrics</Navbar.Brand></Link>
+      <Link to='/'><Navbar.Brand><img src={logo}/> metrics</Navbar.Brand></Link>
     </Navbar>
   );
 }
